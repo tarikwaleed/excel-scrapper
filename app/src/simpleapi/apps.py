69c110679aa3b8a4fe9faa02c943f6ea5a6b5d86
tarikwaleed <tarikwaleed.tech@gmail.com>
2024-08-17 23:06:21 +0300

    :fire:

--- app/src/simpleapi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SimpleapiConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "simpleapi"
--- app/src/simpleapi/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import SimpleView
-
-urlpatterns = [
-    path("", view=SimpleView.as_view()),
-]
